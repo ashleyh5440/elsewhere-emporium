65806cda52ddde3e58f950ea0b4b3dbdab2e6e25
Ashley <ashleyh5440@gmail.com>
2025-06-20 11:32:15 -0400

    changed name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); //importing Mongoose library
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/third-eye-designs');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/moonblood-studio');
 
 module.exports = mongoose.connection; 
