c8009c9925b3d5141dc5eff750260292621089bd
Ashley <ashleyh5440@gmail.com>
2025-06-20 13:14:32 -0400

    updated models wrote mutations and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,32 @@ export const QUERY_USER = gql`
         }
     }
 `;
+
+export const QUERY_USERCART = gql `
+    query getUserCarts($userId: ID!) {
+        getUserDecks(user_id: $userId) {
+            _id
+            user_id
+            cart_name
+            products {
+                _id
+                name
+                category
+                price
+                image
+            }
+        }
+    }
+`;
+
+export const QUERY_PRODUCTS = gql`
+    query getProducts {
+        getProducts {
+        _id
+        name
+        category
+        price
+        image
+        }
+    }
+`;
