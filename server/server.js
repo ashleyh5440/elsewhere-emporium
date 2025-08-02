6a53724c2914dca0fc240ff289f112be32b742ff
Ashley <ashleyh5440@gmail.com>
2025-08-02 09:29:14 -0400

    trying to deploy part 4

--- server/server.js
+++ server/server.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require('express');   //importing Express.js
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
+const db = require('./config/connection');   //importing database connection module
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const models = require('./models')   //importing models
+
+const PORT = process.env.PORT || 3001;   //setting port number for server, defaulting to 3001 if not provided
+const app = express();   //creating Express application instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const startApolloServer = async () => {
-  await server.start();
-
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
-  
-  // Important for MERN Setup: When our application runs from production, it functions slightly differently than in development
-  // In development, we run two servers concurrently that work together
-  // In production, our Node server runs and delivers our client-side bundle from the dist/ folder 
-  if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.use(express.static(path.join(__dirname, '../client/build')));
+const startApolloServer = async () => { //starts the apollo server
+    await server.start();
     
-    app.get('*', (req, res) => {
-      // res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    app.use(express.urlencoded({ extended: false }));   //middleware to parse urlencoded request bodies
+    app.use(express.json()); //middleware to parse JSON request bodies
+    
+      
+    if (process.env.NODE_ENV === 'production') { //graphQL middleware
+      app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      });
+    } 
+      app.use('/graphql', expressMiddleware(server));
+      
+      app.get("/", (req, res) => { //test route to make sure everything's working
+      res.send("Working");
     });
   }
-  
-  // Important for MERN Setup: Any client-side requests that begin with '/graphql' will be handled by our Apollo Server
-  app.use('/graphql', expressMiddleware(server));
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+db.once('open', () => {   //event listener on db (line 3), listens for the 'open' event/database connection is successful
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      });
     });
-  });
-};
+
 
 startApolloServer();
+
