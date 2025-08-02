5ae986bb2e1fded6e19d76026809a3f39c7b3d6e
Ashley <ashleyh5440@gmail.com>
2025-08-02 11:19:56 -0400

    testing database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/moonblood-studio');
+console.log(process.env.MONGODB_URI, "if you see me, this means connection is working");
 
 module.exports = mongoose.connection;
