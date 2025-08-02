c65341b11858263a87a9cd82f8a0157bd45adb90
Ashley <ashleyh5440@gmail.com>
2025-08-02 09:17:29 -0400

    trying to deploy part 3

--- server/server.js
+++ server/server.js
@@ -27,7 +27,8 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, '../client/build')));
     
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      // res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
   }
   
